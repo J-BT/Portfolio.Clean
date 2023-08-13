1709beb3435b170edfe7f98fb506f4c0e1561380
J-BT <jeanbaptiste.theroulde@gmail.com>
2023-08-13 15:17:56 +0200

    + Common/BaseEntity.cs

---
+++ Portfolio.Clean.Domain/Common/BaseEntity.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Portfolio.Clean.Domain.Common;
+
+public class BaseEntity
+{
+    #region Attributes & Properties
+
+
+    #endregion
+
+    #region Constructors
+
+    #endregion
+
+    #region Methods
+
+    #endregion
+}
