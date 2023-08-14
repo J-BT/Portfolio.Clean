57690497317ee2a0dd5241d4eb645f328e461ca8
J-BT <jeanbaptiste.theroulde@gmail.com>
2023-08-14 23:36:20 +0200

    Application : + CreateContactEmail command + handler

---
+++ Portfolio.Clean.Application/Features/ContactEmail/Commands/CreateContactEmail/CreateContactEmailCommand.cs
@@ -0,0 +1,29 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Portfolio.Clean.Application.Features.ContactEmail.Commands.CreateContactEmail;
+
+public class CreateContactEmailCommand : IRequest<int>
+{
+
+    #region Attributes & Accessors
+    public string ContactEmailObject { get; set; } = string.Empty;
+    public string ContactEmailContent { get; set; } = string.Empty;
+    public string ContactEmailSender { get; set; } = string.Empty;
+    public bool ContactEmailIsSent { get; set; }
+    public DateTime? CreationDate { get; set; }
+    public DateTime? LastUpdate { get; set; }
+    #endregion
+
+    #region Constructors
+
+    #endregion
+
+    #region Methods
+
+    #endregion
+}
---
+++ Portfolio.Clean.Application/Features/ContactEmail/Commands/CreateContactEmail/CreateContactEmailCommandHandler.cs
@@ -0,0 +1,46 @@
+﻿using AutoMapper;
+using MediatR;
+using Portfolio.Clean.Application.Contracts.Persistence;
+using Portfolio.Clean.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Portfolio.Clean.Application.Features.ContactEmail.Commands.CreateContactEmail;
+
+public class CreateContactEmailCommandHandler : IRequestHandler<CreateContactEmailCommand, int>
+{
+    private readonly IMapper _mapper;
+    private readonly IContactEmailRepository _contactEmailRepository;
+
+    #region Attributes & Accessors
+
+    #endregion
+
+    #region Constructors
+    public CreateContactEmailCommandHandler(IMapper mapper, IContactEmailRepository contactEmailRepository)
+    {
+        _mapper = mapper;
+        _contactEmailRepository = contactEmailRepository;
+    }
+    #endregion
+
+    #region Methods
+    public async Task<int> Handle(CreateContactEmailCommand request, CancellationToken cancellationToken)
+    {
+        //Validate incoming data
+
+        //Convert to domain entity object
+        var contactEmailToCreate = _mapper.Map<Domain.ContactEmail>(request);
+
+        //Add to database
+        await _contactEmailRepository.CreateAsync(contactEmailToCreate);
+
+        //Return record id
+        return contactEmailToCreate.Id;
+    }
+    #endregion
+
+}
