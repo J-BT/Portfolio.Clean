160f717858b13469f47d811e645e0e6dda3a79fe
Jean-Baptiste Theroulde <jbtheroulde.exterieur@naval-group.com>
2023-08-25 17:27:38 +0200

    Infrastructure (Persistence) : + ProjectConfiguration + TechnologyConfiguration

---
+++ Portfolio.Clean.Persistence/Configurations/ProjectConfiguration.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Portfolio.Clean.Domain;
+
+namespace Portfolio.Clean.Persistence.Configurations;
+
+public class ProjectConfiguration : IEntityTypeConfiguration<Project>
+{
+
+    #region Attributes & Accessors
+
+    #endregion
+
+    #region Constructors
+
+    #endregion
+
+    #region Methods
+    public void Configure(EntityTypeBuilder<Project> builder)
+    {
+        builder.HasData(
+            new Project
+            {
+                Id = 1, 
+                ProjectName = "This first Project is a test for db initialization",
+                TechnologyId = 1,
+                LastUpdate = DateTime.Now,
+                CreationDate = DateTime.Now
+            }
+        );
+
+
+    }
+    #endregion
+}
