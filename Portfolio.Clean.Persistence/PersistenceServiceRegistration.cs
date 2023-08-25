af87895c53a1435a573e4a5b3e18359a1f997dcf
Jean-Baptiste Theroulde <jbtheroulde.exterieur@naval-group.com>
2023-08-25 10:57:02 +0200

    Initialization of Infrastructure/Portfolio.Clean.Persistence

---
+++ Portfolio.Clean.Persistence/PersistenceServiceRegistration.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Portfolio.Clean.Persistence;
+
+public static class PersistenceServiceRegistration
+{
+
+    #region Attributes & Accessors
+
+    #endregion
+
+    #region Constructors
+
+    #endregion
+
+    #region Methods
+    public static IServiceCollection AddPersistenceServices(this IServiceCollection services)
+    {
+        return services;
+    }
+    #endregion
+}
