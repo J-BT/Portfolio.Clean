ebb18b77c4abaa4f6f32b556ba744b9101ca1b09
Jean-Baptiste Theroulde <jbtheroulde.exterieur@naval-group.com>
2023-08-28 23:13:39 +0200

    Infrastructure : Creation of InfrastructureServicesRegistation.cs

---
+++ Portfolio.Clean.Infrastructure/InfrastructureServicesRegistration.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Portfolio.Clean.Infrastructure;
+
+public static class InfrastructureServicesRegistration
+{
+
+	#region Attributes & Accessors
+
+	#endregion
+
+	#region Methods
+	public static IServiceCollection ConfigureInfrastuctureServices(this IServiceCollection services,
+		IConfiguration configuration)
+	{
+		return services;
+	}
+	#endregion
+}
