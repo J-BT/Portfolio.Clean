012ab23a3056bfcd7cb0f085700eaaae20c6d349
Jean-Baptiste Theroulde <jbtheroulde.exterieur@naval-group.com>
2023-09-06 23:42:36 +0200

    BlazorUI : Creation of ServiceClient.cs via NSwagStudio + Extension of IClient & Client

---
+++ Portfolio.Clean.BlazorUI/Services/Base/BaseHttpService.cs
@@ -0,0 +1,22 @@
+﻿namespace Portfolio.Clean.BlazorUI.Services.Base;
+
+public class BaseHttpService
+{
+
+    #region Attributes & Accessors
+
+    protected IClient _client;
+
+    #endregion
+
+    #region Constructors
+    public BaseHttpService(IClient client)
+    {
+        _client = client;
+    }
+    #endregion
+
+    #region Methods
+
+    #endregion
+}
---
+++ Portfolio.Clean.BlazorUI/Services/Base/Client.cs
@@ -0,0 +1,26 @@
+﻿namespace Portfolio.Clean.BlazorUI.Services.Base;
+
+public partial class Client : IClient
+{
+
+	#region Attributes & Accessors
+
+    public HttpClient HttpClient 
+    { 
+        get
+        {
+            return _httpClient;
+        }
+    
+    }
+
+    #endregion
+
+    #region Constructors
+
+    #endregion
+
+    #region Methods
+
+    #endregion
+}
