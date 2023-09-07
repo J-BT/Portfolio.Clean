46df2a7959527f9e07b5e4bf165a38c36db297d2
Jean-Baptiste Theroulde <jbtheroulde.exterieur@naval-group.com>
2023-09-07 17:10:55 +0200

    BlazorUI : Pages/ContactEmails/index.razor OK API : Program.cs update -> +app.UseCors("all");

---
+++ Portfolio.Clean.BlazorUI/Pages/ContactEmails/Index.razor.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.AspNetCore.Components;
+using Portfolio.Clean.BlazorUI.Contracts;
+using Portfolio.Clean.BlazorUI.Models.ContactEmails;
+
+namespace Portfolio.Clean.BlazorUI.Pages.ContactEmails;
+
+public partial class Index
+{
+
+    #region Attributes & Accessors
+    [Inject]
+    public NavigationManager NavigationManager { get; set; }
+
+    [Inject]
+    public IContactEmailService ContactEmailService { get; set; }
+
+    public List<ContactEmailVM> ContactEmails { get; private set; }
+    public string Message { get; set; } = string.Empty;
+
+    #endregion
+
+    #region Constructors
+
+    #endregion
+
+    #region Methods
+
+    protected override async Task OnInitializedAsync()
+    {
+        ContactEmails = await ContactEmailService.GetContactEmails();
+    }
+    public void CreateContactEmail()
+    {
+        NavigationManager.NavigateTo("/contactemails/create/");
+    }
+
+    protected void EditContactEmail(int id)
+    {
+        NavigationManager.NavigateTo($"/contactemails/edit/{id}");
+    }
+
+    protected void DetailsContactEmail(int id)
+    {
+        NavigationManager.NavigateTo($"/contactemails/details/{id}");
+    }
+
+    protected async Task DeleteContactEmail(int id)
+    {
+        var response = await ContactEmailService.DeleteContactEmail(id);
+        if (response.Success)
+        {
+            StateHasChanged();
+        }
+        else
+        {
+            Message = response.Message;
+        }
+    }
+    #endregion
+}
