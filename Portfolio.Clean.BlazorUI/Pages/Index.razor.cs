70c3b9f770f07fbc061656dc17d282543731a0c0
Jean-Baptiste Theroulde <jbtheroulde.exterieur@naval-group.com>
2023-09-07 17:29:13 +0200

    ToDo : Home page (index)

---
+++ Portfolio.Clean.BlazorUI/Pages/Index.razor.cs
@@ -0,0 +1,17 @@
+﻿namespace Portfolio.Clean.BlazorUI.Pages;
+
+public partial class Index
+{
+
+	#region Attributes & Accessors
+
+	#endregion
+
+	#region Constructors
+
+	#endregion
+
+	#region Methods
+
+	#endregion
+}
