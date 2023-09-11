6abadfdeae6985dc7b51dab2952df39c94fafc34
Jean-Baptiste Theroulde <jbtheroulde.exterieur@naval-group.com>
2023-09-11 16:29:28 +0200

    BlazorUI : footer

---
+++ Portfolio.Clean.BlazorUI/Components/Common/PortfolioFooter.razor.cs
@@ -0,0 +1,21 @@
+﻿namespace Portfolio.Clean.BlazorUI.Components.Common;
+
+public partial class PortfolioFooter
+{
+
+	#region Attributes & Accessors
+	public int Year { get; set; }
+    #endregion
+
+    #region Constructors
+
+    #endregion
+
+    #region Methods
+
+    protected override void OnInitialized()
+    {
+        Year = DateTime.Now.Year;
+    }
+    #endregion
+}
