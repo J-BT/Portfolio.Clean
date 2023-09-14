af88c0a0c9ede9adcc4a37c7f98a60b83f4162ed
Jean-Baptiste Theroulde <jbtheroulde.exterieur@naval-group.com>
2023-09-14 16:37:46 +0200

    BlazorUI : Home -> Sun

---
+++ Portfolio.Clean.BlazorUI/Components/Home/Sun.razor.cs
@@ -0,0 +1,17 @@
+﻿namespace Portfolio.Clean.BlazorUI.Components.Home;
+
+public partial class Sun
+{
+
+	#region Attributes & Accessors
+
+	#endregion
+
+	#region Constructors
+
+	#endregion
+
+	#region Methods
+
+	#endregion
+}
