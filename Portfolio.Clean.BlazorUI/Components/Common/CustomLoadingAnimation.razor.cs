ee0a0f973bde97a62803f120977bcc5cda2bd294
Jean-Baptiste Theroulde <jbtheroulde.exterieur@naval-group.com>
2023-09-16 12:15:51 +0200

    BlazorUI : CustomLoadingAnimation WIP

---
+++ Portfolio.Clean.BlazorUI/Components/Common/CustomLoadingAnimation.razor.cs
@@ -0,0 +1,16 @@
+﻿namespace Portfolio.Clean.BlazorUI.Components.Common;
+public partial class CustomLoadingAnimation
+{
+
+    #region Attributes & Accessors
+
+    #endregion
+
+    #region Constructors
+
+    #endregion
+
+    #region Methods
+
+    #endregion
+}
