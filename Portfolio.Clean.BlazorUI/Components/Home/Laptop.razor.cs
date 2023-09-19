d33a72e218aac5db263d67e5d68674aa953efec7
Jean-Baptiste Theroulde <jbtheroulde.exterieur@naval-group.com>
2023-09-19 05:19:14 +0200

    BlazorUI : Laptop Typing WIP

--- Portfolio.Clean.BlazorUI/Components/Home/Laptop.razor.cs
+++ Portfolio.Clean.BlazorUI/Components/Home/Laptop.razor.cs
@@ -1,12 +1,11 @@
-﻿
-
-namespace Portfolio.Clean.BlazorUI.Components.Home;
+﻿namespace Portfolio.Clean.BlazorUI.Components.Home;
 
 public partial class Laptop
 {
 
     #region Attributes & Accessors
-
+    private List<string> LaptopText { get; set; } = new();
+    private string TypingText { get; set; } = string.Empty;
     #endregion
 
     #region Constructors
@@ -16,7 +15,39 @@ public partial class Laptop
     #region Methods
     //protected override async Task OnInitializedAsync()
     //{
+    //    //await base.OnInitializedAsync().ConfigureAwait(false);
+    //    //Thread typingThread = new Thread(new ThreadStart(Typing));
 
+    //}
+
+    //private static void Typing()
+    //{
+    //    while (true)
+    //    {
+    //        var texts = new List<string>
+    //        { "Applications web", "Logiciels", "Sites vitrines"};
+    //        var laptop = new Laptop(texts);
+
+    //        laptop.TypeText();
+
+    //        Thread.Sleep(5000);
+
+
+    //    }
+    //}
+
+    //private async void TypeText()
+    //{
+    //    foreach (string text in LaptopText)
+    //    {
+    //        TypingText += text;
+    //        await Task.Delay(1000);
+    //        //foreach (char letter in text)
+    //        //{
+    //        //    TypingText += letter;
+    //        //    await Task.Delay(100);
+    //        //}
+    //    }
     //}
     #endregion
 }
