ae9a37c6bcea7af953e785391db8db3f3d4e607f
Jean-Baptiste Theroulde <jeanbaptiste.theroulde@gmail.com>
2023-09-20 15:16:20 +0200

    BlazorUI : Laptop -> typing effect OK WIP

--- Portfolio.Clean.BlazorUI/Components/Home/Laptop.razor.cs
+++ Portfolio.Clean.BlazorUI/Components/Home/Laptop.razor.cs
@@ -8,13 +8,11 @@ public partial class Laptop
 {
 
     #region Attributes & Accessors
-    private List<string> LaptopText { get; set; } = new();
-    private string TypingText { get; set; } = string.Empty;
 
     [Inject]
-    public IJSRuntime JSRuntime { get; set; }
+    public IJSRuntime? JSRuntime { get; set; }
 
-    private IJSObjectReference? module;
+    private IJSObjectReference? module { get; set; }
     #endregion
 
     #region Constructors
@@ -25,50 +23,11 @@ public partial class Laptop
 
     protected override async Task OnAfterRenderAsync(bool firstRender)
     {
-        
         if (firstRender)
         {
-            module = await JSRuntime.InvokeAsync<IJSObjectReference>("import", "./Components/Home/Laptop.razor.js");
-
-            await module.InvokeVoidAsync("exampleFunction1");
+            module = await JSRuntime!.InvokeAsync<IJSObjectReference>("import", "./Components/Home/Laptop.razor.js");
+            await module.InvokeVoidAsync("typeOnLaptop");
         }
-
     }
-    //protected override async Task OnInitializedAsync()
-    //{
-    //    //await base.OnInitializedAsync().ConfigureAwait(false);
-    //    //Thread typingThread = new Thread(new ThreadStart(Typing));
-
-    //}
-
-    //private static void Typing()
-    //{
-    //    while (true)
-    //    {
-    //        var texts = new List<string>
-    //        { "Applications web", "Logiciels", "Sites vitrines"};
-    //        var laptop = new Laptop(texts);
-
-    //        laptop.TypeText();
-
-    //        Thread.Sleep(5000);
-
-
-    //    }
-    //}
-
-    //private async void TypeText()
-    //{
-    //    foreach (string text in LaptopText)
-    //    {
-    //        TypingText += text;
-    //        await Task.Delay(1000);
-    //        //foreach (char letter in text)
-    //        //{
-    //        //    TypingText += letter;
-    //        //    await Task.Delay(100);
-    //        //}
-    //    }
-    //}
     #endregion
 }
