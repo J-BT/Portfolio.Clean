d72b9051b4faf570c44913925be1731b9c1647d8
Jean-Baptiste Theroulde <jeanbaptiste.theroulde@gmail.com>
2023-09-22 15:06:10 +0200

    BlazorUI : Creation of the page 'Projects' + component Window WIP

---
+++ Portfolio.Clean.BlazorUI/Pages/Projects/Project.razor.cs
@@ -0,0 +1,16 @@
+﻿namespace Portfolio.Clean.BlazorUI.Pages.Projects;
+
+public partial class Project
+{
+	#region Attributes & Accessors
+
+	#endregion
+
+	#region Constructors
+
+	#endregion
+
+	#region Methods
+
+	#endregion
+}
