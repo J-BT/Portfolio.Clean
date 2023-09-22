8d582e6919a50e5e7d1e7f204606b56f0bd898fe
Jean-Baptiste Theroulde <jeanbaptiste.theroulde@gmail.com>
2023-09-22 16:30:30 +0200

    BlazorUI : Projects -> set positions of ProjectDescription & ProjectTechnologies + Common Component -> SetPageTitle

---
+++ Portfolio.Clean.BlazorUI/Components/Common/SetPageTitle.razor.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace Portfolio.Clean.BlazorUI.Components.Common;
+
+public partial class SetPageTitle
+{
+
+    #region Attributes & Accessors
+    [Parameter]
+    public string PageTitleName { get; set; } = "Page title";
+    #endregion
+
+    #region Constructors
+
+    #endregion
+
+    #region Methods
+
+    #endregion
+}
