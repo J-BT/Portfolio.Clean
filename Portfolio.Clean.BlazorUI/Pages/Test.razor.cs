79ba9e5a4200923b3fdac2692a36db62833a732f
Jean-Baptiste Theroulde <jeanbaptiste.theroulde@gmail.com>
2023-09-27 16:31:52 +0200

    BlazorUI : loacal storage testOK

---
+++ Portfolio.Clean.BlazorUI/Pages/Test.razor.cs
@@ -0,0 +1,17 @@
+﻿namespace Portfolio.Clean.BlazorUI.Pages;
+
+public partial class Test
+{
+
+	#region Attributes & Accessors
+
+	#endregion
+
+	#region Constructors
+
+	#endregion
+
+	#region Methods
+
+	#endregion	
+}
