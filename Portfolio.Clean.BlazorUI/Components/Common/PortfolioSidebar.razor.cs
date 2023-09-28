fc648a5909d66550416214769918811d4314ae62
Jean-Baptiste Theroulde <jeanbaptiste.theroulde@gmail.com>
2023-09-28 11:20:26 +0200

    BlazorUI : multilingual Sidebar

--- Portfolio.Clean.BlazorUI/Components/Common/PortfolioSidebar.razor.cs
+++ Portfolio.Clean.BlazorUI/Components/Common/PortfolioSidebar.razor.cs
@@ -1,4 +1,9 @@
-﻿namespace Portfolio.Clean.BlazorUI.Components.Common;
+﻿using AKSoftware.Localization.MultiLanguages;
+using Microsoft.AspNetCore.Components;
+using Microsoft.JSInterop;
+using System.Globalization;
+
+namespace Portfolio.Clean.BlazorUI.Components.Common;
 
 public partial class PortfolioSidebar
 {
@@ -7,7 +12,12 @@ public partial class PortfolioSidebar
 
     private string projectsList = string.Empty;
     private string aboutList = string.Empty;
-    
+    [Inject]
+    private ILanguageContainerService LanguageContainer { get; set; }
+    [Inject]
+    public IJSRuntime JS { get; set; }
+    public string ActualLanguage { get; set; } = string.Empty;
+
     #endregion
 
     #region Constructors
@@ -16,10 +26,18 @@ public partial class PortfolioSidebar
 
     #region Methods
 
-    protected override void OnInitialized()
+    protected override async Task OnInitializedAsync()
     {
         projectsList = "none";
         aboutList = "none";
+
+        ActualLanguage = await JS.InvokeAsync<string>("localStorage.getItem", "language");
+
+        if (!String.IsNullOrEmpty(ActualLanguage))
+        {
+            LanguageContainer.SetLanguage(CultureInfo.GetCultureInfo(ActualLanguage));
+
+        }
     }
 
     private void ProjectDropdown()
