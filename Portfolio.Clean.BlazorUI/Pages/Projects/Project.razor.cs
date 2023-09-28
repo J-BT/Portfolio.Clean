c0431f3341e2e5c1fd72eb7fcbb22cde2a70a3ce
Jean-Baptiste Theroulde <jeanbaptiste.theroulde@gmail.com>
2023-09-28 15:42:37 +0200

    BlazorUI : Yml files indentation

--- Portfolio.Clean.BlazorUI/Pages/Projects/Project.razor.cs
+++ Portfolio.Clean.BlazorUI/Pages/Projects/Project.razor.cs
@@ -1,10 +1,20 @@
-﻿namespace Portfolio.Clean.BlazorUI.Pages.Projects;
+﻿using AKSoftware.Localization.MultiLanguages;
+using Microsoft.AspNetCore.Components;
+using Microsoft.JSInterop;
+using System.Globalization;
+
+namespace Portfolio.Clean.BlazorUI.Pages.Projects;
 
 public partial class Project
 {
     #region Attributes & Accessors
     private string DescriptionTxt { get; set; } = string.Empty;
     private string Technologies { get; set; } = string.Empty;
+    [Inject]
+    private ILanguageContainerService LanguageContainer { get; set; }
+    [Inject]
+    public IJSRuntime JS { get; set; }
+    public string ActualLanguage { get; set; } = string.Empty;
     #endregion
 
     #region Constructors
@@ -16,6 +26,14 @@ public partial class Project
     {
         DescriptionTxt = "Description du projet";
         Technologies = "Liste des technologies (.NET, angular, etc)";
+
+        ActualLanguage = await JS.InvokeAsync<string>("localStorage.getItem", "language");
+
+        if (!String.IsNullOrEmpty(ActualLanguage))
+        {
+            LanguageContainer.SetLanguage(CultureInfo.GetCultureInfo(ActualLanguage));
+
+        }
 
         await base.OnInitializedAsync();
     }
