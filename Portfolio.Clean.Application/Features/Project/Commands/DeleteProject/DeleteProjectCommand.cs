f4ee04802bb2d216e4d33f632c18a459564d1faf
Jean-Baptiste Theroulde <jeanbaptiste.theroulde@gmail.com>
2023-09-29 15:05:22 +0200

    Application : new feature -> DeleteProject

---
+++ Portfolio.Clean.Application/Features/Project/Commands/DeleteProject/DeleteProjectCommand.cs
@@ -0,0 +1,26 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Portfolio.Clean.Application.Features.Project.Commands.DeleteProject;
+
+public class DeleteProjectCommand : IRequest<Unit>
+{
+
+	#region Attributes & Accessors
+
+	public string ProjectName { get; set; } = string.Empty;
+
+	#endregion
+
+	#region Constructors
+
+	#endregion
+
+	#region Methods
+
+	#endregion
+}
---
+++ Portfolio.Clean.Application/Features/Project/Commands/DeleteProject/DeleteProjectCommandHandler.cs
@@ -0,0 +1,48 @@
+﻿using MediatR;
+using Portfolio.Clean.Application.Contracts.Persistence;
+using Portfolio.Clean.Application.Exceptions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Portfolio.Clean.Application.Features.Project.Commands.DeleteProject;
+
+public class DeleteProjectCommandHandler : IRequestHandler<DeleteProjectCommand, Unit>
+{
+
+	#region Attributes & Accessors
+
+	private readonly IProjectRepository _projectRepository;
+
+	#endregion
+
+	#region Constructors
+	public DeleteProjectCommandHandler(IProjectRepository projectRepository)
+    {
+		_projectRepository = projectRepository;
+	}
+    #endregion
+
+    #region Methods
+
+    public async Task<Unit> Handle(DeleteProjectCommand request, CancellationToken cancellationToken)
+	{
+		// Retrive domain entity object
+		var projectToDelete = await _projectRepository.GetProjectWithDetails(request.ProjectName);
+
+		// Verify that record exists
+		if (projectToDelete == null)
+			throw new NotFoundException(nameof(Project), request.ProjectName);
+
+		// Remove record from database
+		await _projectRepository.DeleteAsync(projectToDelete);
+
+		// Return value unit
+		return Unit.Value;
+	}
+
+	#endregion
+
+}
