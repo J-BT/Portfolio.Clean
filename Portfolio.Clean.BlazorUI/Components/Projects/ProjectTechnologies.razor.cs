462cdfdd69e1ebf37facb81bb945735396b24161
Jean-Baptiste Theroulde <jeanbaptiste.theroulde@gmail.com>
2023-10-02 02:01:21 +0200

    BlazorUI : Project -> SetTechnologiesImg() WIP

--- Portfolio.Clean.BlazorUI/Components/Projects/ProjectTechnologies.razor.cs
+++ Portfolio.Clean.BlazorUI/Components/Projects/ProjectTechnologies.razor.cs
@@ -8,6 +8,8 @@ public partial class ProjectTechnologies
     #region Attributes & Accessors
     [Parameter]
     public string Technologies { get; set; } = string.Empty;
+    public List<string> TechnologiesIcons { get; set; } = new();
+    public List<string> TechnologiesIconsAlt { get; set; } = new();
     #endregion
 
     #region Constructors
@@ -15,6 +17,40 @@ public partial class ProjectTechnologies
     #endregion
 
     #region Methods
+
+    protected override async Task OnInitializedAsync()
+    {
+        SetTechnologiesImg();
+
+        await base.OnInitializedAsync();
+    }
+    private void SetTechnologiesImg()
+    {
+        if (Technologies.Contains(","))
+        {
+            string t = Technologies.Replace(" ", "");
+            TechnologiesIconsAlt = t.Split(",").ToList();
+
+            foreach (var technology in TechnologiesIconsAlt)
+            {
+                
+                if (ImageExists())
+                {
+                    TechnologiesIcons.Add(@$"/images/technologies/{technology}.svg");
+
+                }
+            }
+        }
+    }
+
+    private bool ImageExists()
+    {
+        /****************************************************************/
+        /************************** TO DO *******************************/
+
+        /*check if image exist here*/
+        return true;
+    }
 
     #endregion
 }
