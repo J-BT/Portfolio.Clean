d3781155f7a6d848ff235352fd04756724493709
Jean-Baptiste Theroulde <jeanbaptiste.theroulde@gmail.com>
2023-10-03 19:46:52 +0200

    BlazorUI : Home  -> +PurpleBlueCircle component, new design WIP

---
+++ Portfolio.Clean.BlazorUI/Components/Home/PurpleBlueCircle.razor.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace Portfolio.Clean.BlazorUI.Components.Home;
+
+public partial class PurpleBlueCircle
+{
+
+    #region Attributes & Accessors
+    [Parameter]
+    public string PositionTop { get; set; } = string.Empty;
+    [Parameter]
+    public string PositionLeft { get; set; } = string.Empty;
+	[Parameter]
+	public string CircleWidth { get; set; } = string.Empty;
+	[Parameter]
+	public string ZIndex { get; set; } = string.Empty;
+	[Parameter]
+	public string Opacity { get; set; } = string.Empty;
+
+	#endregion
+
+	#region Constructors
+
+	#endregion
+
+	#region Methods
+
+	#endregion
+}
