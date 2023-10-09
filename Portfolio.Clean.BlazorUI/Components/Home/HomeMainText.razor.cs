749fc61cda0710f0ad3eb9271bf75115924b2510
Jean-Baptiste Theroulde <jeanbaptiste.theroulde@gmail.com>
2023-10-09 18:13:20 +0200

    BlazorUI : + components -> HomeMainText & HomeBottomText

---
+++ Portfolio.Clean.BlazorUI/Components/Home/HomeMainText.razor.cs
@@ -0,0 +1,16 @@
+﻿namespace Portfolio.Clean.BlazorUI.Components.Home;
+
+public partial class HomeMainText
+{ 
+	#region Attributes & Accessors
+
+	#endregion
+
+	#region Constructors
+
+	#endregion
+
+	#region Methods
+
+	#endregion
+}
