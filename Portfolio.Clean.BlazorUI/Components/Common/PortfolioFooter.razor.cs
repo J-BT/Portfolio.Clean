c18ef8863e90a00c403c71c0740342b9ae4dbf40
Jean-Baptiste Theroulde <jeanbaptiste.theroulde@gmail.com>
2023-10-10 11:48:55 +0200

    BlazorUI : LanguageContainer set to footer

--- Portfolio.Clean.BlazorUI/Components/Common/PortfolioFooter.razor.cs
+++ Portfolio.Clean.BlazorUI/Components/Common/PortfolioFooter.razor.cs
@@ -1,10 +1,20 @@
-﻿namespace Portfolio.Clean.BlazorUI.Components.Common;
+﻿using AKSoftware.Localization.MultiLanguages;
+using Microsoft.AspNetCore.Components;
+using Microsoft.JSInterop;
+using System.Globalization;
+
+namespace Portfolio.Clean.BlazorUI.Components.Common;
 
 public partial class PortfolioFooter
 {
 
-	#region Attributes & Accessors
-	public int Year { get; set; }
+    #region Attributes & Accessors
+    [Inject]
+    private ILanguageContainerService LanguageContainer { get; set; }
+    [Inject]
+    public IJSRuntime JS { get; set; }
+    public string ActualLanguage { get; set; } = string.Empty;
+    public int Year { get; set; }
     #endregion
 
     #region Constructors
@@ -13,9 +23,18 @@ public partial class PortfolioFooter
 
     #region Methods
 
-    protected override void OnInitialized()
+    protected override async Task OnInitializedAsync()
     {
         Year = DateTime.Now.Year;
+
+        ActualLanguage = await JS.InvokeAsync<string>("localStorage.getItem", "language");
+
+        if (!String.IsNullOrEmpty(ActualLanguage))
+        {
+            LanguageContainer.SetLanguage(CultureInfo.GetCultureInfo(ActualLanguage));
+
+        }
+
     }
     #endregion
 }
