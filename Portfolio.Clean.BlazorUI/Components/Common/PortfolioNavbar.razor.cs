ba68a3a317800aed233904cb47761a3c4e424612
Jean-Baptiste Theroulde <jeanbaptiste.theroulde@gmail.com>
2023-10-10 10:19:46 +0200

    BlazorUI : Navbar (exp toggle) + Footer OK

---
+++ Portfolio.Clean.BlazorUI/Components/Common/PortfolioNavbar.razor.cs
@@ -0,0 +1,68 @@
+﻿using AKSoftware.Localization.MultiLanguages;
+using Microsoft.AspNetCore.Components;
+using Microsoft.JSInterop;
+using System.Globalization;
+
+namespace Portfolio.Clean.BlazorUI.Components.Common;
+
+public partial class PortfolioNavbar
+{
+
+    #region Attributes & Accessors
+
+    private string projectsList = string.Empty;
+    private string aboutList = string.Empty;
+    [Inject]
+    private ILanguageContainerService LanguageContainer { get; set; }
+    [Inject]
+    public IJSRuntime JS { get; set; }
+    public string ActualLanguage { get; set; } = string.Empty;
+
+    #endregion
+
+    #region Constructors
+
+    #endregion
+
+    #region Methods
+
+    protected override async Task OnInitializedAsync()
+    {
+        projectsList = "none";
+        aboutList = "none";
+
+        ActualLanguage = await JS.InvokeAsync<string>("localStorage.getItem", "language");
+
+        if (!String.IsNullOrEmpty(ActualLanguage))
+        {
+            LanguageContainer.SetLanguage(CultureInfo.GetCultureInfo(ActualLanguage));
+
+        }
+    }
+
+    private void ProjectDropdown()
+	{
+        if(projectsList == "none")
+        {
+            projectsList = "block";
+        }
+        else
+        {
+            projectsList = "none";
+        }
+    }
+
+    private void AboutDropdown()
+    {
+        if (aboutList == "none")
+        {
+            aboutList = "block";
+        }
+        else
+        {
+            aboutList = "none";
+        }
+    }
+
+    #endregion
+}
