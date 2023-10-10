af57c400c7694beb7ef5c512f81463b9bc85b8ee
Jean-Baptiste Theroulde <jeanbaptiste.theroulde@gmail.com>
2023-10-10 16:40:30 +0200

    BlazorUI : Solution card WIP

---
+++ Portfolio.Clean.BlazorUI/Components/Solutions/SolutionsCards.razor.cs
@@ -0,0 +1,17 @@
+﻿namespace Portfolio.Clean.BlazorUI.Components.Solutions;
+
+public partial class SolutionsCards
+{
+
+	#region Attributes & Accessors
+
+	#endregion
+
+	#region Constructors
+
+	#endregion
+
+	#region Methods
+
+	#endregion
+}
