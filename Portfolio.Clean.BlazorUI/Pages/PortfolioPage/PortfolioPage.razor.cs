4300b7745d845d1caa25c7e85d50986e581067d3
Jean-Baptiste Theroulde <jeanbaptiste.theroulde@gmail.com>
2023-10-15 02:11:00 +0200

    BlazorUI : + PortfolioPage -> new background

---
+++ Portfolio.Clean.BlazorUI/Pages/PortfolioPage/PortfolioPage.razor.cs
@@ -0,0 +1,34 @@
+﻿using AKSoftware.Localization.MultiLanguages;
+using Microsoft.AspNetCore.Components;
+using Portfolio.Clean.BlazorUI.Contracts.Helpers;
+
+namespace Portfolio.Clean.BlazorUI.Pages.PortfolioPage;
+
+public partial class PortfolioPage
+{
+
+    #region Attributes & Accessors
+
+    private bool isLoaded = false;
+    [Inject]
+    private ILanguage Language { get; set; }
+    private ILanguageContainerService LanguageContainer { get; set; }
+    #endregion
+
+    #region Constructors
+
+    #endregion
+
+    #region Methods
+    protected override async Task OnInitializedAsync()
+    {
+        //Language settings
+        LanguageContainer = Language.GetResourceFile();
+        await Language.GetLanguageFromBrowserAsync();
+
+        await Task.Delay(800);
+        isLoaded = true;
+        await base.OnInitializedAsync();
+    }
+    #endregion
+}
